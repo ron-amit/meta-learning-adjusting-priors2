8dd4f933ed834217da3babd5085a2293338326e0
Ron Amit <amitron5@gmail.com>
2019-04-30 15:04:21 +0300

    Update complexity_terms.py
    corrected the complexity terms

--- Utils/complexity_terms.py
+++ Utils/complexity_terms.py
@@ -12,28 +12,46 @@ from Utils.common import net_weights_magnitude, count_correct
 # -----------------------------------------------------------------------------------------------------------#
 
 
-def get_hyper_divergnce(prm, prior_model):
+def get_hyper_divergnce(prm, prior_model):   # corrected
     ''' calculates a divergence between hyper-prior and hyper-posterior....
      which is, in our case, just a regularization term over the prior parameters  '''
 
     # Note:  the hyper-prior is N(0, kappa_prior^2 * I)
     # Note:  the hyper-posterior is N(parameters-of-prior-distribution, kappa_post^2 * I)
 
-    # KLD between hyper-posterior and hyper-prior:
-    hyper_dvrg = (1 / (2 * prm.kappa_prior ** 2)) * net_weights_magnitude(prior_model, prm, p=2)
+    kappa_post = prm.kappa_post
+    kappa_prior = prm.kappa_prior
 
+    if prm.divergence_type == 'KL':
+        # KLD between hyper-posterior and hyper-prior:
+        norm_sqr = net_weights_magnitude(prior_model, prm, p=2)
+        hyper_dvrg = (norm_sqr + kappa_post**2) / (2 * kappa_prior**2) + math.log(kappa_prior / kappa_post) - 1/2
+
+    elif prm.divergence_type == 'W_NoSqr':
+        d = prior_model.weights_count
+        hyper_dvrg = torch.sqrt(net_weights_magnitude(prior_model, prm, p=2) + d * (kappa_prior - kappa_post) ** 2)
+
+    elif prm.divergence_type == 'W_Sqr':
+        d = prior_model.weights_count
+        hyper_dvrg = net_weights_magnitude(prior_model, prm, p=2) + d * (kappa_prior - kappa_post) ** 2
+
+    else:
+        raise ValueError('Invalid prm.divergence_type')
+
+    assert hyper_dvrg >= 0
     return hyper_dvrg
 
 
 # -----------------------------------------------------------------------------------------------------------#
 
-def get_meta_complexity_term(hyper_kl, prm, n_train_tasks):
+def get_meta_complexity_term(hyper_kl, prm, n_train_tasks):   # corrected
     if n_train_tasks == 0:
         meta_complex_term = 0.0  # infinite tasks case
     else:
-        if prm.complexity_type == 'McAllester' or  prm.complexity_type == 'Seeger':
+        if prm.complexity_type in {'McAllester', 'Seeger'}:
             delta = prm.delta
-            meta_complex_term = torch.sqrt(hyper_kl / (2*n_train_tasks) + math.log(4*math.sqrt(n_train_tasks) / delta))
+            meta_complex_term = torch.sqrt(
+                (hyper_kl + math.log(2 * n_train_tasks / delta)) / (2 * (n_train_tasks - 1)))  # corrected
 
         elif prm.complexity_type == 'PAC_Bayes_Pentina':
             meta_complex_term = hyper_kl / math.sqrt(n_train_tasks)
@@ -49,10 +67,9 @@ def get_meta_complexity_term(hyper_kl, prm, n_train_tasks):
     return meta_complex_term
 # -----------------------------------------------------------------------------------------------------------#
 
-#  -------------------------------------------------------------------------------------------
-#  Intra-task complexity for posterior distribution
-# -------------------------------------------------------------------------------------------
-def get_task_complexity(prm, prior_model, post_model, n_samples, avg_empiric_loss, hyper_dvrg=0, n_train_tasks=1, dvrg=None, noised_prior=False):
+
+def get_task_complexity(prm, prior_model, post_model, n_samples, avg_empiric_loss, hyper_dvrg=0, n_train_tasks=1, dvrg=None, noised_prior=False):   # corrected
+    #  Intra-task complexity for posterior distribution
 
     complexity_type = prm.complexity_type
     delta = prm.delta  #  maximal probability that the bound does not hold
@@ -67,12 +84,12 @@ def get_task_complexity(prm, prior_model, post_model, n_samples, avg_empiric_los
 
     elif prm.complexity_type == 'McAllester':
         # According to 'Simplified PAC-Bayesian Margin Bounds', McAllester 2003
-        # complex_term = torch.sqrt((1 / (2 * (n_samples-1))) * (hyper_dvrg + div + math.log(2 * n_samples / delta)))
-        complex_term = torch.sqrt((hyper_dvrg + dvrg + math.log(2 * n_samples / delta)) / (2 * (n_samples - 1)))
+        complex_term = torch.sqrt((hyper_dvrg + dvrg + math.log(2 * n_samples * n_train_tasks / delta)) / (2 * (n_samples - 1)))  # corrected
 
     elif prm.complexity_type == 'Seeger':
         # According to 'Simplified PAC-Bayesian Margin Bounds', McAllester 2003
-        seeger_eps = (dvrg + hyper_dvrg + math.log(2 * math.sqrt(n_samples) / delta)) / n_samples
+        seeger_eps = (dvrg + hyper_dvrg + math.log(2 * n_train_tasks * math.sqrt(n_samples) / delta)) / n_samples # corrected
+
         sqrt_arg = 2 * seeger_eps * avg_empiric_loss
         # sqrt_arg = F.relu(sqrt_arg)  # prevent negative values due to numerical errors
         complex_term = 2 * seeger_eps + torch.sqrt(sqrt_arg)
@@ -80,7 +97,7 @@ def get_task_complexity(prm, prior_model, post_model, n_samples, avg_empiric_los
     elif prm.complexity_type == 'Catoni':
         # See "From PAC-Bayes Bounds to KL Regularization" Germain 2009
         # & Olivier Catoni. PAC-Bayesian surpevised classification: the thermodynamics of statistical learning
-        complex_term = avg_empiric_loss + (2 / n_samples) * (hyper_dvrg + dvrg + math.log(1/ delta))
+        complex_term = avg_empiric_loss + (2 / n_samples) * (hyper_dvrg + dvrg + math.log(n_train_tasks/ delta))  # corrected
 
 
     # elif prm.complexity_type == 'Seeger2':
@@ -121,6 +138,8 @@ def get_net_densities_divergence(prior_model, post_model, prm, noised_prior=Fals
         if hasattr(prior_layer, 'b'):
             total_dvrg += get_dvrg_element(post_layer.b, prior_layer.b, prm, noised_prior)
 
+    if prm.divergence_type == 'W_NoSqr':
+        total_dvrg = torch.sqrt(total_dvrg)
 
     return total_dvrg
 # -------------------------------------------------------------------------------------------
@@ -140,14 +159,21 @@ def  get_dvrg_element(post, prior, prm, noised_prior=False):
     post_std = torch.exp(0.5 * post['log_var'])
     prior_std = torch.exp(0.5 * prior_log_var)
 
+    if  prm.divergence_type in ['W_Sqr', 'W_NoSqr']:
+            # Wasserstein norm with p=2
+            # according to DOWSON & LANDAU 1982
+            div_elem = torch.sum((post['mean'] - prior_mean).pow(2) + (post_std - prior_std).pow(2))
 
-   # Calculate KL divergence between two Gaussian vectors:
-    numerator = (post['mean'] - prior_mean).pow(2) + post_var
-    denominator = prior_var
-    div_elem = 0.5 * torch.sum(prior_log_var - post['log_var'] + numerator / denominator - 1)
+    elif prm.divergence_type == 'KL':
+        numerator = (post['mean'] - prior_mean).pow(2) + post_var
+        denominator = prior_var
+        div_elem = 0.5 * torch.sum(prior_log_var - post['log_var'] + numerator / denominator - 1)
+    else:
+        raise ValueError('Invalid prm.divergence_type')
 
     # note: don't add small number to denominator, since we need to have zero KL when post==prior.
 
+    assert div_elem >= 0
     return div_elem
 # -------------------------------------------------------------------------------------------
 
